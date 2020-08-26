593bd706e541bdfb95799c2c81aab64dfa66ac63
aurelie-p <aurelie-p@users.noreply.github.com>
2020-08-26 14:58:04 +0200

    test post-commit hook

--- src/sum.js
+++ src/sum.js
@@ -1,1 +1,12 @@
 // Implement the sum function here
+
+function getSum(a, b) {
+    if (isNaN(a) || isNaN(b)) {
+        throw new Error('Parameter is not a number!');
+    }
+    return a + b;
+}
+
+module.exports = getSum;
+
+//getSum('Hello', 5);
--- test/sum.test.js
+++ test/sum.test.js
@@ -1,1 +1,16 @@
 // Add the unit tests of the sum function here
+
+describe('Sum', function() {
+
+    const sum = require('../src/sum');
+
+    test('adds 1 + 2 to equal 3', () => {
+        expect(sum(1, 2)).toBe(3);
+    });
+
+    test('invalid arguments', () => {
+        expect(() => {
+            sum(1, "x");
+        }).toThrow();
+    });
+});
